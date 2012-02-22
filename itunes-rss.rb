eeda2d2b3e1e449dded91a6b021f3c46751c9910
Victor Mota <vimota@gmail.com>
2012-02-21 21:46:13 -0500

    Updated to work using symlinks and corrected urls with '.' in their name

--- itunes-rss.rb
+++ itunes-rss.rb
@@ -1,23 +1,25 @@
 require "rss/maker"
 require "FileUtils"
 
-BASE_PATH 	= Dir.home + "/Music/iTunes/iTunes Media/iTunes U"
+BASE_PATH 	= Dir.home + "/Music/iTunes/iTunes Media/iTunes U/"
 VERSION 	= "2.0"
-BASE_URL	= "http://192.168.0.191/"
+BASE_URL	= "http://192.168.0.191/iTunesU"
 BASE_SRV	= "/Library/WebServer/Documents/"
 
 def urlize text
-	text.gsub(" ", "-").gsub(",", "_")
+	url = text.gsub(" ", "-").gsub(",", "_")
+	(1..url.count('.') - 1).each do
+		url[url.index('.')] = '-'
+	end
+	url
 end
 
-Dir.glob(BASE_PATH + "/*").each do |folder|
+Dir.glob(BASE_PATH + "*").each do |folder|
 	feed_name = folder.match(/iTunes U\/(.*)/)[1]
 	dest	  = urlize(feed_name) + ".xml"
-
-	FileUtils.cp_r(folder, BASE_SRV)
-	puts "Copied the directory #{feed_name} and its files into webserver."
+	
 	begin
-		FileUtils.mv(BASE_SRV + feed_name, BASE_SRV + urlize(feed_name)) #Renames folders
+		FileUtils.mv(BASE_PATH + feed_name, BASE_PATH + urlize(feed_name)) #Renames folders
 	rescue
 		puts "Error renaming folder (may have already been renamed): " + folder
 	end
@@ -27,8 +29,8 @@ Dir.glob(BASE_PATH + "/*").each do |folder|
 		m.channel.link = "http://www.vimota.me"
 		m.channel.description = "iTunes U feed acquired through itunes-rss.rb by Victor Mota"
 
-		Dir.glob(BASE_PATH + "/" + feed_name + "/*").each do |file|
-			if file.end_with? ".m4v"
+		Dir.glob(BASE_PATH + feed_name + "/*").each do |file|
+			if file.end_with? ".m4v" or file.end_with? ".mp4"
 				file_name 	= file.match(/#{feed_name}\/(.*)/)[1]
 				link 	  	= BASE_URL + urlize(feed_name) + "/" + urlize(file_name)
 
@@ -41,7 +43,7 @@ Dir.glob(BASE_PATH + "/*").each do |folder|
 				i.enclosure.length = File.stat(file).size
 				i.enclosure.type = 'video/x-m4v'
 				begin
-					FileUtils.mv(BASE_SRV + urlize(feed_name) + "/" + file_name, BASE_SRV + urlize(feed_name) + "/" + urlize(file_name))
+					FileUtils.mv(BASE_PATH + urlize(feed_name) + "/" + file_name, BASE_PATH + urlize(feed_name) + "/" + urlize(file_name))
 				rescue
 				end
 			end
